ea90682a49c0deec0f846b32733835b8e3741590
yolux.wang <yolux.wang@beseye.com>
2014-03-14 14:19:46 +0800

    I want to test again

--- themovie.rb
+++ themovie.rb
@@ -22,13 +22,62 @@ File.open(path, "r:utf-8") do |f|
 		imdb = infos[6].strip
 		folder = infos[7].strip
 		output << "<li><a href=\"#{imdb}\" data-btdate=\"#{release}\" data-category=\"#{category}\" data-size=\"#{size}G\" data-copypath=\"~/#{folder}\" data-largesrc=\"images/m#{id}.jpg\" data-title=\"#{eng_name}\" data-description=\"#{cht_name}\"><img src=\"images/s#{id}.jpg\" alt=\"#{eng_name}\"/></a></li>"
+		
 	end
-    puts "html file output"
+  puts "html file output"
 	puts output.size
 	output.reverse!
-	output.each {|x| puts x}
-    puts "save to txt"
-	File.open("#{path}.txt", 'w+') {|f| f.write output.join("\n")}
+
+	html1 = <<XXY
+		<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>watch list</title>
+		<meta name="description" content="The Movie" />
+		<meta name="keywords" content="movie" />
+		<meta name="author" content="Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico"> 
+		<link rel="stylesheet" type="text/css" href="css/default.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<script src="js/modernizr.custom.js"></script>
+	</head>
+	<body>
+		<div class="container">	
+			
+			<header class="clearfix">
+				<!-- <h1>The Movie<span>watch list</span></h1>	 -->
+				<h1>The Movie</h1>	
+				<h2>watch list</h2>
+			</header>
+			<div class="main">
+				<ul id="og-grid" class="og-grid">
+XXY
+html2 = <<YXX
+		</ul>
+			</div>
+		</div><!-- /container -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="js/grid.js"></script>
+		<script>
+			$(function() {
+				Grid.init();
+			});
+		</script>
+	</body>
+</html>
+YXX
+	final = []
+	final << html1
+	final.concat output
+	final << html2
+	final.each {|x| puts x}
+    puts "save to index.html"
+	# File.open("#{path}.txt", 'w+') {|f| f.write output.join("\n")}
+	File.open("#{File.dirname(path)}/index.html", 'w+') {|f| f.write final.join("\n")}
+
 end
 
 rescue => e
